f5cdc7f370c82fa1c9a2f6b560197a7b448d9533
Gayoung-Seo <gayoungs9397@gmail.com>
2019-01-25 17:34:41 -0600

    Update adding leap year

--- src/jia/calendar/Calendar.java
+++ src/jia/calendar/Calendar.java
@@ -5,9 +5,19 @@ import java.util.Scanner;
 public class Calendar {
 
     private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    public int getMaxDaysOfMonths(int month){
-        return MAX_DAYS[month-1];
+    public boolean isLeapYear(int year){
+        if(year%4 == 0 && (year % 100 != 0 || year % 400 ==0))
+            return true;
+        else return false;
+    }
+
+    public int getMaxDaysOfMonths(int year, int month){
+        if(isLeapYear(year)){
+            return LEAP_MAX_DAYS[month-1];
+        }
+        else return MAX_DAYS[month-1];
     }
 
     public String printMonth(int month){
@@ -65,7 +75,8 @@ public class Calendar {
         System.out.println("<< "+printMonth(month-1)+"      "+printMonth(month)+"      "+printMonth(month+1)+" >>");
         System.out.println("Sun Mon Tue Wed Thu Fri Sat");
 
-        int maxDay = getMaxDaysOfMonths(month);
+        int maxDay = getMaxDaysOfMonths(year,month);
+
         for(int i= 1; i<=maxDay;i++){
             System.out.printf("%4d", i);
             if(i % 7 == 0){
--- src/jia/calendar/Prompt.java
+++ src/jia/calendar/Prompt.java
@@ -4,21 +4,25 @@ import java.util.Scanner;
 
 public class Prompt {
 
-    private final static String PROMPT="month> ";
-
     public static void runPrompt(){
 
         Scanner input = new Scanner(System.in);
         Calendar cal = new Calendar();
         int month = 1;
+        int year = 0;
+
         while(true){
+            System.out.println("연도를 입력하세요");
+            System.out.print("YEAR> ");
+            year =input.nextInt();
+
             System.out.println("달을 입력하세요");
-            System.out.printf(PROMPT);
+            System.out.print("MONTH> ");
             month = input.nextInt();
             if(month<1 || month>12){
                 break;
             }
-            cal.printCalendar(2017, month);
+            cal.printCalendar(year, month);
         }
         input.close();
     }
